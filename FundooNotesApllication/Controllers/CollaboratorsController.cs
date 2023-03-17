9d98e32519d6abe53a4e1cdde547a0f942fc81d5
Supriya2922 <supriyaprakash2229@gmail.com>
2023-03-17 19:46:51 +0530

    [Supriya]-Created Collab APIs in repository and manager

---
+++ FundooNotesApllication/Controllers/CollaboratorsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FundooNotesApllication.Controllers
+{
+    public class CollaboratorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
