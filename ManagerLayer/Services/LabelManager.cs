1fe47e8a8bfd0a4b2a230109a89c04beb14a0a94
Supriya2922 <supriyaprakash2229@gmail.com>
2023-03-20 12:49:52 +0530

    [Supriya]-Added label functions in LabelManager class

--- ManagerLayer/Services/LabelManager.cs
+++ ManagerLayer/Services/LabelManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ModelLayer;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +9,31 @@ namespace ManagerLayer.Services
 {
     public class LabelManager
     {
+        private readonly ILabelRepository repository;
+        public LabelManager(ILabelRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public LabelEntity AddLabel(AddLabelModel model, long userid)
+        {
+            return repository.AddLabel(model, userid);
+        }
+        public bool RemoveLabel(long userid, long noteid, long labelid)
+        {
+            return repository.RemoveLabel(userid, noteid, labelid);
+        }
+        public LabelEntity UpdateLabel(long userid, UpdateLabelModel model)
+        {
+            return repository.UpdateLabel(userid, model);
+        }
+        public IEnumerable<LabelEntity> GetLabels(long userid, long noteid)
+        {
+            return repository.GetLabels(userid, noteid);
+        }
+        public IEnumerable<LabelEntity> GetAllLabelsForUser(long userid)
+        {
+            return repository.GetAllLabelsForUser(userid);
+        }
     }
 }
