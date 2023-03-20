2b847c91ba6cefb78706c30f28178c82d8063241
Supriya2922 <supriyaprakash2229@gmail.com>
2023-03-20 12:01:33 +0530

    [Supriya]-Added addLabel function in repository

--- RepositoryLayer/Services/LabelRepository.cs
+++ RepositoryLayer/Services/LabelRepository.cs
@@ -1,10 +1,48 @@
-﻿using System;
+﻿using ModelLayer;
+using RepositoryLayer.Entity;
+using RepositoryLayer.FundooDBContext;
+using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 
 namespace RepositoryLayer.Services
 {
-    internal class LabelRepository
+    public class LabelRepository
     {
+        private readonly FunContext context;
+
+        public LabelRepository(FunContext context)
+        {
+            this.context = context;
+
+        }
+        public LabelEntity AddLabel(AddLabelModel model,long userid)
+        {
+            try
+            {
+                LabelEntity label = new LabelEntity();
+                label.UserId= userid;
+                label.NotesId = model.NoteId;
+                label.LabelName = model.LabelName;
+                var check=context.Labels.Add(label);
+                context.SaveChanges();
+                if(check!= null)
+                {
+                    return label;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
     }
 }
