d59c1115bb73b1f90aa7a1f46026d82e13e43cb0
Supriya2922 <supriyaprakash2229@gmail.com>
2023-03-20 13:02:47 +0530

    [Supriya]-Added addLabel in controller

---
+++ FundooNotesApllication/Controllers/LabelController.cs
@@ -0,0 +1,43 @@
+﻿using ManagerLayer.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ModelLayer;
+using RepositoryLayer.Entity;
+using System;
+
+namespace FundooNotesApllication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LabelController : ControllerBase
+    {
+        private readonly ILabelManager manager;
+        public LabelController(ILabelManager manager)
+        {
+            this.manager = manager;
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult addLabel(AddLabelModel model)
+        {
+            try
+            {
+                var userid = Convert.ToInt64(User.FindFirst("Id").Value.ToString());
+                var labeladded=manager.AddLabel(model,userid);
+                if(labeladded!=null)
+                {
+                    return Ok(new ResponseModel<LabelEntity> { Status=true ,Message="Label added successfully",Data= labeladded});
+                }
+                else
+                {
+                    return BadRequest(new ResponseModel<string> { Status = true, Message = "Label could not be added" });
+                }
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
