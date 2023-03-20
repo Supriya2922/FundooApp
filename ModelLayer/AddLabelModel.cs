b77bd0217acf48fedf5f4fd3d0d2d418c6267165
Supriya2922 <supriyaprakash2229@gmail.com>
2023-03-20 12:11:41 +0530

    [Supriya]-Added UpdateLabel model in repository

---
+++ ModelLayer/AddLabelModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer
+{
+    public class AddLabelModel
+    {
+        public long NoteId { get; set; }
+        public string LabelName { get; set; }
+    }
+}
---
+++ ModelLayer/UpdateLabelModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLayer
+{
+    public class UpdateLabelModel
+    {
+        public long labelid { get; set; }
+        public long NotesId { get; set; }
+        public string labelname { get; set; }
+       
+
+
+    }
+}
